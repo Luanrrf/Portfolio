db5f3494f08add4f0526e9ab6ca8683f23b5ec71
Luan Ribeiro <luanrrf@yahoo.com.br>
2024-08-20 08:40:33 -0300

    feat: starting on mobile version

--- src/app/components/Introduction/index.tsx
+++ src/app/components/Introduction/index.tsx
@@ -16,22 +16,22 @@ const Introduction = () => {
   return (
     <section className={`${style.introduction} Screen`} id="Introduction">
       <div className={style.introductionTexts}>
-        <h2 className="text-6xl leading-tight">
+        <h2 className="text-2xl lg:text-6xl leading-tight lg:leading-normal">
           {introductionTexts?.["Title"][0]}
         </h2>
-        <h1 className="text-6xl leading-tight">
+        <h1 className="text-2xl lg:text-6xl leading-tight lg:leading-normal">
           {introductionTexts?.["Title"][1]}&nbsp;
           <span className={`${style.name} ${style.typeWriter}`} id="typewriter">
             Luan Ribeiro
           </span>
         </h1>
 
-        <div className="text-2xl mt-4">
+        <div className={`${style.introductionSubtitle} text-1xl lg:text-3xl leading-normal`}>
           {introductionTexts.Subtitle.map((subtitle) => (
             <p key={subtitle}>{subtitle}</p>
           ))}
         </div>
-        <div className="text-1xl">
+        <div className="text-1xl lg:text-2xl leading-relaxed mt-4 lg:mt-8">
           {introductionTexts.Description.map((text) => (
             <p key={text}>{text}</p>
           ))}
